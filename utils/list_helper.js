d5bb38a62f28faf601e1101178869f2df8153525
Jens Pitkanen <jens.pitkanen@helsinki.fi>
2018-11-24 22:50:58 +0200

    Add 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,10 @@ const totalLikes = (blogs) => {
   return blogs.map(blog => blog.likes).reduce((a, b) => a + b, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  return blogs.sort((a, b) => b.likes - a.likes)[0]
+}
+
 module.exports = {
-  totalLikes
+  totalLikes, favoriteBlog
 }
